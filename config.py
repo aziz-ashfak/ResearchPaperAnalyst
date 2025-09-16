a929544a1966a06d129d44507cf9399321854151
AzizAshfak <azizashfak@gmail.com>
2025-09-16 17:43:43 +0600

    update

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
-
+from dotenv import find_dotenv,load_dotenv
+load_dotenv(find_dotenv())
 # API Keys
-os.environ["GROQ_API_KEY"] = "YOUR_API_KEY"
-
+GROQ_API_KEY = os.environ['GROQ_API_KEY']
 # Directories
 IMAGE_DIR = "extracted_images"
 os.makedirs(IMAGE_DIR, exist_ok=True)
--- src/utils/llm_utils.py
+++ src/utils/llm_utils.py
@@ -1,17 +1,21 @@
 import base64
+import os
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage
-#from src.prompts import summarize_prompt
 
+from dotenv import find_dotenv,load_dotenv
+load_dotenv(find_dotenv())
+_ = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = os.environ['GROQ_API_KEY']
 # --- Image Description ---
 def describe_image(image_path, image_ext):
-    """Generate scientific description of an image using Groq Vision LLM."""
+    """Generate scientific description of an image using Groq Vision  LLM."""
     with open(image_path, "rb") as image_file:
         base64_image = base64.b64encode(image_file.read()).decode("utf-8")
 
     mime_type = f"image/{image_ext}" if image_ext != "jpg" else "image/jpeg"
-    llm_vision = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct"
+    llm_vision = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct" # model 
                           #model="llama-3.3-70b-versatile"
                           )
 
